8f8cfac5fd3f17d6a8fd8824a72957f2f25e615c
Licshee <licshee@bilibili.com>
2015-07-03 23:30:09 +0800

    added CanRun interfaces added DuplexCommandBase

---
+++ OmniKits.Interfaces/CanRunInterfaces.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace OmniKits
+{
+    public interface ICanRun
+    {
+        void Run();
+    }
+
+    public interface ICanRunAsync
+    {
+        Task RunAsync(CancellationToken cancellationToken);
+    }
+
+    public interface ICanRun<T>
+    {
+        T Run();
+    }
+
+    public interface ICanRunAsync<T>
+    {
+        Task<T> RunAsync(CancellationToken cancellationToken);
+    }
+
+    public interface ICanRunDuplex : ICanRun, ICanRunAsync
+    { }
+
+    public interface ICanRunDuplex<T> : ICanRun<T>, ICanRunAsync<T>
+    { }
+
+}
---
+++ OmniKits.Tasks/DuplexCommandBase.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OmniKits.Threading.Tasks
+{
+    public abstract class DuplexCommandBase : ICanRunDuplex
+    {
+        private bool _IsRunOverridden = true;
+        private bool _IsRunAsyncOverridden = true;
+
+        private void EnsureOverridden()
+        {
+            if (_IsRunOverridden || _IsRunAsyncOverridden)
+                return;
+            throw new NotImplementedException();
+        }
+
+        public virtual void Run()
+        {
+            _IsRunOverridden = false;
+            EnsureOverridden();
+
+            Task.Factory.StartNew(() => RunAsync(CancellationToken.None)).Unwrap().Wait();
+            //TaskEx.Run(() => RunAsync(CancellationToken.None)).Wait();
+        }
+
+        public virtual Task RunAsync(CancellationToken cancellationToken)
+        {
+            if (cancellationToken.CanBeCanceled)
+                throw new NotSupportedException();
+
+            _IsRunAsyncOverridden = false;
+            EnsureOverridden();
+
+            return Task.Factory.StartNew(Run);
+        }
+    }
+
+    public abstract class DuplexCommandBase<T> : ICanRunDuplex<T>
+    {
+        private bool _IsRunOverridden = true;
+        private bool _IsRunAsyncOverridden = true;
+
+        private void EnsureOverridden()
+        {
+            if (_IsRunOverridden || _IsRunAsyncOverridden)
+                return;
+            throw new NotImplementedException();
+        }
+
+        public virtual T Run()
+        {
+            _IsRunOverridden = false;
+            EnsureOverridden();
+
+            return Task.Factory.StartNew(() => RunAsync(CancellationToken.None)).Unwrap().Result;
+            //return TaskEx.Run(() => RunAsync(CancellationToken.None)).Result;
+        }
+
+        public virtual Task<T> RunAsync(CancellationToken cancellationToken)
+        {
+            if (cancellationToken.CanBeCanceled)
+                throw new NotSupportedException();
+
+            _IsRunAsyncOverridden = false;
+            EnsureOverridden();
+
+            return Task<T>.Factory.StartNew(Run);
+        }
+    }
+}
