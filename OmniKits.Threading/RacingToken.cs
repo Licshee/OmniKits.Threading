694023efb3be45f5456b14e0f792dd979868ae01
NanaLich <NanaLich@users.noreply.github.com>
2015-07-10 03:21:47 +0800

    added RacingToken & support for more platforms

---
+++ OmniKits.Threading/RacingToken.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+
+namespace OmniKits.Threading
+{
+    public struct RacingToken
+    {
+        private volatile Thread _Owner;
+        public bool IsCaptured => _Owner != null;
+        public bool IsOwnedByCurrentThread => _Owner == Thread.CurrentThread;
+
+        public bool TryCapture()
+        {
+            if (IsCaptured)
+                return false;
+
+            return Interlocked.CompareExchange(ref _Owner, Thread.CurrentThread, null) == null;
+        }
+
+        public void Release()
+        {
+            var owner = _Owner;
+
+            if (_Owner != Thread.CurrentThread)
+                throw new InvalidOperationException();
+
+            _Owner = null;
+        }
+#if !PORTABLE
+        public bool ForceRelease()
+        {
+            var owner = _Owner;
+
+            if (owner == null)
+                return false;
+
+            var isForeign = (owner == Thread.CurrentThread);
+
+            if (isForeign && owner.IsAlive)
+                throw new InvalidOperationException();
+
+            return Interlocked.CompareExchange(ref _Owner, null, owner) == owner && isForeign;
+        }
+#endif
+    }
+}
